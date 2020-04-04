ecebba2a2f7f60dfd90c761e173dca1ee131b03a
Daniel Rauch <dan.rauch@yahoo.de>
2020-04-05 01:00:23 +0200

    refactor python code and update doc

--- rpi-receiver/garduino_receiver/garduino_receiver.py
+++ rpi-receiver/garduino_receiver/garduino_receiver.py
@@ -1,47 +1,45 @@
 #!/usr/bin/env python3
 
-from bluepy import btle
-from influxdb import InfluxDBClient
+from collections import namedtuple
+from datetime import datetime
 import time
 import json
-from datetime import datetime
+from json import JSONDecodeError
+from influxdb import InfluxDBClient
+from bluepy import btle
 
-ADDRESS = 'F0:F8:F2:C3:6C:35'
-CONNECTIVITY_SERVICE_UUID = '0000ffe0-0000-1000-8000-00805f9b34fb'
-STORE_DATA_CHARACTERISTICS_UUID = '0000ffe1-0000-1000-8000-00805f9b34fb'
+ValueAndType = namedtuple('ValueAndType', ['value', 'type'])
+
+DEVICE_ADDRESS = 'F0:F8:F2:C3:6C:35'
 influxdb_client = InfluxDBClient(host='127.0.0.1', port=8086, username='****',
-                                 password='****', database='somedbname')
+                                 password='****', database='pyexample')
 
 
-class MyDelegate(btle.DefaultDelegate):
+class ReceivedDataDelegate(btle.DefaultDelegate):
+
     def __init__(self, params):
-        btle.DefaultDelegate.__init__(self)      
-        
-    def handleNotification(self, cHandle, data):
-        json_data = {}
+        btle.DefaultDelegate.__init__(self)
+        # map received ids from Arduino to the corresponding DB table entries and types
+        self._valuemap = {'t': ValueAndType('temperature_celcius_main', float),
+                          'p': ValueAndType('pressure_hpa', float),
+                          'rh': ValueAndType('relative_humidity_percent', int),
+                          't2': ValueAndType('temperature_celcius_sec', int),
+                          's0': ValueAndType('soil_moisture_percent_0', int),
+                          's1': ValueAndType('soil_moisture_percent_1', int),
+                          's2': ValueAndType('soil_moisture_percent_2', int),
+                          'li': ValueAndType('light_intensity_percent', int),}
+
+    # override handleNotification to handle received data
+    def handleNotification(self, hanlde, data):
         try:
             json_data = json.loads(data.decode('utf-8'))
-        except:
-            print("Could not decode JSON - skipping data entrys")
+        except JSONDecodeError as e:
+            print(f'Could not decode JSON - skipping data entrys. Error {repr(e)}')
             return
-        data_dict = {}     
-        for key, data in json_data.items():
-            if key == 't':
-                data_dict['temperature_celcius_main'] = data
-            if key == 'p':
-                data_dict['pressure_hpa'] = data
-            if key == 'rh':
-                data_dict['relative_humidity_percent'] = data
-            if key == 't2':
-                data_dict['temperature_celcius_sec'] = data
-            if key == 's0':
-                data_dict['soil_moisture_percent_0'] = data
-            if key == 's1':
-                data_dict['soil_moisture_percent_1'] = data
-            if key == 's2':
-                data_dict['soil_moisture_percent_2'] = data
-            if key == 'li':
-                data_dict['light_intensity_percent'] = data
+        data_dict = {}
+        for datakey, datavalue in json_data.items():
+            valueandtype = self._valuemap[datakey]
+            data_dict[valueandtype.value] = valueandtype.type(datavalue)
 
         current_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
         json_body = [
@@ -54,25 +52,21 @@ class MyDelegate(btle.DefaultDelegate):
                 "time": current_time,
                 "fields": data_dict
             }
-        ]
+        ]        
         influxdb_client.write_points(json_body)
 
 
-def init_ble_peripheral():
-    peripheral = btle.Peripheral(ADDRESS, btle.ADDR_TYPE_PUBLIC)
-    peripheral.setDelegate(MyDelegate(1))
-    # services = peripheral.getServiceByUUID(CONNECTIVITY_SERVICE_UUID)
-    # characteristics = services.getCharacteristics(forUUID=STORE_DATA_CHARACTERISTICS_UUID)
-    return peripheral
-
-def listen_for_ble_data(peripheral):
-    while True:
-        if peripheral.waitForNotifications(10.0):
-            # handleNotification() was called
-            continue
-
+def receive_ble_data():
+    with btle.Peripheral(DEVICE_ADDRESS, btle.ADDR_TYPE_PUBLIC) as peripheral:
+        peripheral.setDelegate(ReceivedDataDelegate(1))
+        try:
+            while True:
+                if peripheral.waitForNotifications(10.0):
+                    # handleNotification() was called
+                    continue
+        except KeyboardInterrupt:
+            print('exit receiver')
 
 
 if __name__ == "__main__":
-    peripheral = init_ble_peripheral()
-    listen_for_ble_data(peripheral)
+    receive_ble_data()
